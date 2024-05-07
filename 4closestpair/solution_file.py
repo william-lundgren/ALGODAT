100e50d057a64d8baf783c3fa1c04fc6cbb156ff
william-lundgren <superbanan@live.se>
2024-05-07 16:04:46 +0200

    ayo working wtf?

--- 4closestpair/solution_file.py
+++ 4closestpair/solution_file.py
@@ -10,6 +10,9 @@ class Point:
     def get_dist(self, other):
         return math.sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2)
 
+    def __repr__(self):
+        return f"{self.x, self.y}"
+
 
 def closest_points(all_points):
     # list sorted by x values
@@ -34,18 +37,26 @@ def closest(p_x, p_y, n):
         d_1 = closest(r_x, r_y, n/2)
         d_2 = closest(l_x, l_y, n/2)
         delta = min(d_1, d_2)
-
+        streck = r_y[0].y
+        s_y = [point for point in p_y if abs(point.y - streck) < delta]
+        for i in range(len(s_y)):
+            for j in range(i + 1, len(s_y)):
+                dist = s_y[i].get_dist(s_y[j])
+                if dist < delta:
+                    delta = dist
+        return delta
     # Fuck it we hardcoding
     else:
         small_path = -1
         for i in range(len(p_x)):
-            for j in range(i, len(p_x)):
+            for j in range(i + 1, len(p_x)):
                 dist = p_x[i].get_dist(p_x[j])
                 if small_path == -1:
                     small_path = dist
                 else:
                     if dist < small_path:
                         small_path = dist
+
         return small_path
 
 
@@ -63,7 +74,8 @@ def main():
         point = Point(x, y)
         points.append(point)
 
-    print(closest_points(points))
+    # TODO remember floating point stuff
+    print("{:.6f}".format(closest_points(points), 6))
 
 
 if __name__ == "__main__":
