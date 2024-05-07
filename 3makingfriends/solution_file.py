5b2a48211a436f76200773af803e3a552b85aaf5
Leo <leo@example.com>
2024-05-07 14:16:15 +0200

    commented stuff :)

--- 3makingfriends/solution_file.py
+++ 3makingfriends/solution_file.py
@@ -13,6 +13,8 @@ class Graph:
         x = self.find(x)
         y = self.find(y)
         
+        #Union by rank (see uniion-find on wikipedia). Essentially we want to keep track of the height of each tree.
+        #The lowest rank tree is added to the higher tree, in order to keep the trees as flat as possible (and avoid long recursive stuff)
         if self.rank[x] < self.rank[y]:
             x, y = (y,x)
         
@@ -25,13 +27,11 @@ class Graph:
         if self.parent[x] == x:
             return x
         else:
-            return self.find(self.parent[x])
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent
+            self.parent[x] = self.find(self.parent[x]) #attempt at "path compression"?
+        return self.parent[x]
 
 
     def kruskal(self):
-        startt = time.time()
         mst = []
         for e in self.edges:
             u = e[0]-1
@@ -43,7 +43,7 @@ class Graph:
 
 
 def main():
-
+    tstart = time.time()
     input_lines = []
     for line in sys.stdin:
         if '' == line.rstrip():
@@ -51,11 +51,13 @@ def main():
         input_lines.append(line.replace("\n", ""))
 
     N, M = [int(i) for i in input_lines.pop(0).split()]
+    sys.stderr.write(f"Setup time: {time.time()-tstart}\n") # ugly way of displaying time stats while not rendering checksolution.sh unusuable
     edges = [[int(e) for e in line.split()] for line in input_lines]
     g = Graph(N, edges)
+    sys.stderr.write(f"Total time before kruskal: {time.time()-tstart}\n")
     mst = g.kruskal()
-    # print(mst)
     s = sum([edge[2] for edge in mst])
+    sys.stderr.write(f"Total time: {time.time()-tstart}\n") 
     print(s)
 
 if __name__ == "__main__":
