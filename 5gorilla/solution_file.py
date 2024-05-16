f29bfed209f4ba0f70042bd5cd7929289b3da9c5
william-lundgren <superbanan@live.se>
2024-05-16 17:12:59 +0200

    add timing

--- 5gorilla/solution_file.py
+++ 5gorilla/solution_file.py
@@ -1,6 +1,13 @@
 import time
 import sys
 
+class Timer0:
+    timer = 0
+class TimerOpt:
+    timer = 0
+class TimerAlgo:
+    timer = 0
+
 
 def parse_input():
     # Input() takes line by line
@@ -43,13 +50,17 @@ def align_strings(s1, s2, gain_map):
 
     # Initiate first row and first column, since only lateral moment is allowed along 0th elements
     # the prev must be from below or to the left respectively and always move through delta
+
+    tstart = time.time()
     for i in range(m + 1):
         prev[i][0] = 2
         OPT[i][0] = delta * i
     for i in range(n + 1):
         prev[0][i] = 1
         OPT[0][i] = delta * i
+    Timer0.timer += time.time() - tstart
 
+    tstart = time.time()
     # Fill OPT matrix
     for i in range(1, m + 1):
         for j in range(1, n + 1):
@@ -64,12 +75,15 @@ def align_strings(s1, s2, gain_map):
             optval, optpath = max((left, 1), (bottom, 2), (diag, 3))
             OPT[i][j] = optval
             prev[i][j] = optpath
+    TimerOpt.timer += time.time() - tstart
 
     # construct strings from opt matrix
     s1_aligned = ''
     s2_aligned = ''
     i, j = m, n
 
+
+    tstart = time.time()
     # While not both i and j are 0 we can still move backwards
     while (i > 0) or (j > 0):
         # Lateral moves means we had to add a character
@@ -89,7 +103,9 @@ def align_strings(s1, s2, gain_map):
             i -= 1
             j -= 1
         else:
-            raise ValueError("Incorrevt prev matrix")
+            raise ValueError("Incorrect prev matrix")
+
+    TimerAlgo.timer += time.time() - tstart
 
     return s1_aligned, s2_aligned
 
@@ -97,10 +113,16 @@ def align_strings(s1, s2, gain_map):
 def main():
     tstart = time.time()
     gain_map, queries = parse_input()
+    sys.stderr.write(f"Parse input: {time.time() - tstart}\n")
+
     for s1, s2 in queries:
         aligned_s1, aligned_s2 = align_strings(s1, s2, gain_map)
         print(aligned_s1, aligned_s2)
+    sys.stderr.write(f"Initialize 0s: {Timer0.timer}\n")
+    sys.stderr.write(f"Fill opt: {TimerOpt.timer}\n")
+    sys.stderr.write(f"Algorithm thing haha leo: {TimerAlgo.timer}\n")
     sys.stderr.write(f"Time: {time.time()-tstart}\n")
+
 
 
 if __name__ == "__main__":
